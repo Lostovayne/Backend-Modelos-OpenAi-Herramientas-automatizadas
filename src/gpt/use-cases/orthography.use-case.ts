f7c152f0dd33559a691a161a3e05a5eca3f06228
LostoVayne <moises.barrera.dev@gmail.com>
2024-05-11 19:07:27 -0400

    fix listo

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -4,7 +4,10 @@ interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (openai: OpenAI, options: Options) => {
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options
+) => {
   const { prompt } = options;
 
   const completion = await openai.chat.completions.create({
@@ -18,7 +21,9 @@ export const orthographyCheckUseCase = async (openai: OpenAI, options: Options) 
         si encuentras algún error  en el texto devuelve un mensaje indicándole al usuario como puede mejorar y también agrega el error al array de la respuesta junto a su solución, 
         Solo felicita al usuario en caso de no haber ningún error en el texto y estar escrito perfectamente
         Debes de responder en formato JSON,
-        Asigna un Puntaje al usuario según el número de errores encontrados en el campo userScore usando un porcentaje en relación al texto
+        Asigna un Puntaje al usuario según el número de errores encontrados en el campo userScore usando un porcentaje en relación al texto.
+        El puntaje debe ser un número entre 0 y 100, sin decimales.
+        ElPorcentaje tiene que tener una breve explicacion de por qué le dió ese puntaje.
    
        
         Ejemplo de salida:
@@ -36,7 +41,6 @@ export const orthographyCheckUseCase = async (openai: OpenAI, options: Options) 
       },
     ],
     model: "gpt-3.5-turbo",
-    max_tokens: 100,
   });
 
   // console.log(completion);
